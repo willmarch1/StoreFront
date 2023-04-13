4252c9c503ba86ad2a8be6da5e11605c46c5927b
Will March <willmarch11@hotmail.com>
2023-04-13 14:54:22 -0500

    implemented ef

---
+++ StoreFront.DATA.EF/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal ProductPrice { get; set; }
+        public int BrandId { get; set; }
+        public int TypeId { get; set; }
+        public int GenId { get; set; }
+        public int StatusId { get; set; }
+        public string? ProductImage { get; set; }
+
+        public virtual Brand Brand { get; set; } = null!;
+        public virtual Gen Gen { get; set; } = null!;
+        public virtual Status Status { get; set; } = null!;
+        public virtual Type Type { get; set; } = null!;
+    }
+}
---
+++ StoreFront.DATA.EF/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreFront.DATA.EF.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
